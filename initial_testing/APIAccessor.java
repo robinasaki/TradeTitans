56c71860110efd67e16e52b1aecb26bb7e30e71f
cebwalton <colinwalton2002@gmail.com>
2023-10-01 18:07:59 -0400

    changed APIAccessor to print entire request instead of only 10 lines

--- initial_testing/APIAccessor.java
+++ initial_testing/APIAccessor.java
@@ -7,7 +7,7 @@ public class APIAccessor {
         File f = new File("key.txt");
         BufferedReader apiKeyReader = new BufferedReader(new FileReader("key.txt"));
         String key = apiKeyReader.readLine();
-        String urlString = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=" + key;
+        String urlString = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=" + key;
         URL url = new URL(urlString);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
@@ -15,15 +15,10 @@ public class APIAccessor {
         System.out.println("Response Code: " + responseCode);
 
         BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        for(int i = 0; i < 10; i++)
-            System.out.println(br.readLine());
-//        StringBuilder response = new StringBuilder();
-//
-//        while ((inputLine = in.readLine()) != null) {
-//            response.append(inputLine);
-//        }
-        br.close();
-//
-//        System.out.println("Response Data:\n" + response.toString());
+        String request = "";
+        String line;
+        while ((line = br.readLine()) != null)
+            request = request + line + "\n";
+        System.out.println(request);
     }
 }
