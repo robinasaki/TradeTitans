b87b4aac3dad101ef44bd1c901ca055581e2546e
Abdulrahman123109 <free.doom99960@gmail.com>
2023-10-26 17:18:42 -0400

    Adding the constructor for Portfolio class

--- stock-tracker-app/src/main/java/entity/Portfolio.java
+++ stock-tracker-app/src/main/java/entity/Portfolio.java
@@ -1,5 +1,15 @@
 package entity;
-
+import Currency;
 public class Portfolio {
+    private final String name;
 
+    private final Currency currency;
+
+    private final int protfolioId;
+
+    public Portfolio(String name, Currency currency, int protfolioId){
+        this.name = name;
+        this.currency = currency;
+        this.protfolioId = protfolioId;
+    }
 }
