537f5f5d7db491b3798d215ed7d470c61bf20382
cebwalton <colinwalton2002@gmail.com>
2023-10-26 17:11:51 -0400

    filled in constructor and getters in Tradable.jave

---
+++ stock-tracker-app/src/main/java/entity/Tradable.java
@@ -0,0 +1,28 @@
+package entity;
+
+import java.util.Date;
+
+public abstract class Tradeable {
+    private String name; // e.g. "Apple Inc."
+    private String symbol; // e.g. "AAPL"
+    private HashMap<Date, double> price_history; // price history in USD
+    
+    public Tradeable(String name, String symbol, double price) {
+        this.name = name;
+        this.symbol = symbol;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public HashMap<Date, double> getPriceHistory() {
+        return price_history;
+    }
+
+}
