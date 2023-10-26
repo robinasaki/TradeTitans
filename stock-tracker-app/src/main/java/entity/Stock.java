bcf892177b371cc3da4de979abab6649eccda0e2
jlpal <jlpalubiski@gmail.com>
2023-10-26 17:33:51 -0400

    added base to Stock class

--- stock-tracker-app/src/main/java/entity/Stock.java
+++ stock-tracker-app/src/main/java/entity/Stock.java
@@ -1,5 +1,11 @@
 package entity;
 
+import java.util.Date;
+import java.util.HashMap;
+
 public class Stock extends Tradeable {
-    
+
+    public Stock(String name, String symbol, HashMap<Date, Double> priceHistory) {
+        super(name, symbol, priceHistory);
+    }
 }
