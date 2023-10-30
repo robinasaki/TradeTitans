e7c8e2bf8c677fc62c6a606516864c6a31d6d6f9
cebwalton <colinwalton2002@gmail.com>
2023-10-30 12:41:59 -0400

    changed implementation of TradeTransaction to not use a share price

--- stock-tracker-app/src/main/java/entity/TradeTransaction.java
+++ stock-tracker-app/src/main/java/entity/TradeTransaction.java
@@ -1,18 +1,35 @@
 package entity;
 
 public class TradeTransaction extends Transaction {
-    Tradeable assetIn;
-    Tradeable assetOut;
-    Double shares;
-    Double sharePrice;
+    private Tradeable assetIn;
+    private Tradeable assetOut;
+    private Double shares_in;
+    private Double shares_out;
 
-    public TradeTransaction(Double tradingFee, Tradeable assetIn, Tradeable assetOut, Double shares, Double sharePrice) {
+    public TradeTransaction(Double tradingFee, Tradeable assetIn, Tradeable assetOut, Double shares)
         super(tradingFee);
         this.assetIn = assetIn;
         this.assetOut = assetOut;
         this.shares = shares;
-        this.sharePrice = sharePrice;
     }
+
+    public Tradeable getAssetIn() {
+        return assetIn;
+    }
+
+    public Tradeable getAssetOut() {
+        return assetOut;
+    }
+
+    public Double getShares_in() {
+        return shares_in;
+    }
+
+    public Double getShares_out() {
+        return shares_out;
+    }
+
+
 
     private void trade() {
         // #TODO
