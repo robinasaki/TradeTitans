9d2ed6a02fe2d492d1fb77ffaedef5b642c0c3a9
cebwalton <colinwalton2002@gmail.com>
2023-10-30 12:24:54 -0400

    made some changes to portfolio

--- stock-tracker-app/src/main/java/entity/Portfolio.java
+++ stock-tracker-app/src/main/java/entity/Portfolio.java
@@ -1,11 +1,21 @@
 package entity;
-import Currency;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+import entity.Currency;
+import entity.Tradeable;
+import entity.Transaction;
+
 public class Portfolio {
     private final String name;
 
     private final Currency currency;
 
     private final int portfolioId;
+
+    private HashMap <Tradeable, Double> holdings;
+    private ArrayList<Transaction> transactions;
 
     public Portfolio(String name, Currency currency, int portfolioId){
         this.name = name;
