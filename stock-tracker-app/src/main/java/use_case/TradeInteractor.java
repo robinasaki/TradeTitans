fcfb7da8b9bba3a0e1aeef1455442daad5df2756
cebwalton <colinwalton2002@gmail.com>
2023-11-01 16:34:31 -0400

    minor reformating

--- stock-tracker-app/src/main/java/use_case/TradeInteractor.java
+++ stock-tracker-app/src/main/java/use_case/TradeInteractor.java
@@ -1,14 +1,18 @@
-package use_cases;
+package use_case;
 
-import use_cases.TradeInputBoundary;
-import use_cases.TradeInputData;
+import use_case.TradeInputBoundary;
+import use_case.TradeInputData;
 import entity.TradeTransaction;
 
 public class TradeInteractor implements TradeInputBoundary{
     
     @Override
-    public void execute(TradeInputData tradeInputData,) {
-        TradeTransaction tradeTransaction = new TradeTransaction(tradeInputData.getTradingFee(), tradeInputData.getAssetIn, tradeInputData.getAssetOut(), tradeInputData.getAmountIn(), tradeInputData.getAmountOut());
+    public void execute(TradeInputData tradeInputData) {
+        TradeTransaction tradeTransaction = new TradeTransaction(tradeInputData.getTradingFee(),
+                                                                 tradeInputData.getAssetIn(),
+                                                                 tradeInputData.getAssetOut(),
+                                                                 tradeInputData.getAmountIn(),
+                                                                 tradeInputData.getAmountOut());
         
     }
 }
