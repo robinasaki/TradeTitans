891ee1262144e3735ec307048a2b7dc3248535c3
jlpal <jlpalubiski@gmail.com>
2023-11-01 16:40:56 -0400

    Added base to TradeInputData

--- stock-tracker-app/src/main/java/use_case/TradeInputData.java
+++ stock-tracker-app/src/main/java/use_case/TradeInputData.java
@@ -1,4 +1,23 @@
 package use_case;
 
 public class TradeInputData {
+    final private String tradingFee;
+    final private String assetIn;
+    final private String assetOut;
+    final private String amountIn;
+    final private String amountOut;
+
+    public TradeInputData(String tradingFee, String assetIn, String assetOut, String amountIn, String amountOut) {
+        this.tradingFee = tradingFee;
+        this.assetIn = assetIn;
+        this.assetOut = assetOut;
+        this.amountIn = amountIn;
+        this.amountOut = amountOut;
+    }
+
+    String getTradingFee() { return tradingFee; }
+    String getAssetIn() { return assetIn; }
+    String getAssetOut() { return assetOut; }
+    String getAmountIn() { return amountIn; }
+    String getAmountOut() { return amountOut; }
 }
