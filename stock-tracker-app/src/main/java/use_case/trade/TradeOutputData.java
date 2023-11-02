fff4114a99c1597aef08c12850472c1df2ba4d4a
jlpal <jlpalubiski@gmail.com>
2023-11-02 19:22:44 -0400

    Added DataAccessInterface for Trade

--- stock-tracker-app/src/main/java/use_case/trade/TradeOutputData.java
+++ stock-tracker-app/src/main/java/use_case/trade/TradeOutputData.java
@@ -1,4 +1,10 @@
 package use_case.trade;
 
 public class TradeOutputData {
+    // TODO: Need to finish this section
+    private boolean useCaseFailed;
+
+    public TradeOutputData(boolean useCaseFailed) {
+        this.useCaseFailed = useCaseFailed;
+    }
 }
