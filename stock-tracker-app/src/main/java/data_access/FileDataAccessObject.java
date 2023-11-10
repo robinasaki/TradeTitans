a798bafb70b6c01882e4463876f5ba8bee39d401
jlpal <jlpalubiski@gmail.com>
2023-11-10 14:42:06 -0500

    fixed minor issues

--- stock-tracker-app/src/main/java/data_access/FileDataAccessObject.java
+++ stock-tracker-app/src/main/java/data_access/FileDataAccessObject.java
@@ -2,12 +2,15 @@ package data_access;
 
 import entity.Portfolio;
 import entity.User;
+import java.util.HashMap;
 
 public class FileUserDataAccessObject {
 
     private final File csvFile;
 
-    private final Map<String, Portfolio> portfolios;
+    private final HashMap<String, Portfolio> portfolios;
 
-    public FileUserDataAccessObject(String csvPath, User user) {}
+    public FileUserDataAccessObject(String csvPath, User user, HashMap<String, Portfolio> portfolios) {
+        this.portfolios = portfolios;
+    }
 }
