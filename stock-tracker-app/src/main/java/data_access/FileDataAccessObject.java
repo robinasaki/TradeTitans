0c02ecb20276b31c5736f780641e7a0fea3d648e
cebwalton <colinwalton2002@gmail.com>
2023-11-12 15:25:50 -0500

    created method for loading all portfolios from data

--- stock-tracker-app/src/main/java/data_access/FileDataAccessObject.java
+++ stock-tracker-app/src/main/java/data_access/FileDataAccessObject.java
@@ -1,11 +1,15 @@
 package data_access;
 
 import entity.Portfolio;
-import entity.User;
-import java.io.File;
-import java.util.HashMap;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FileUserDataAccessObject {
+public class FileDataAccessObject {
     private static final String FILE_PATH = "portfolioData.ser";
 
     public void savePortfolios(List<Portfolio> portfolios) {
@@ -14,5 +18,15 @@ public class FileUserDataAccessObject {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public List<Portfolio> loadPortfolios() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH))) {
+            return (List<Portfolio>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return new ArrayList<>();
     }
 }
