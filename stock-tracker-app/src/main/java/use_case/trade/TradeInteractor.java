e3e22fdc56d7e1dc1b2be7677e8c1b18057a1b7a
jlpal <jlpalubiski@gmail.com>
2023-11-12 12:20:48 -0500

    Added main body to TradeInteractor

--- stock-tracker-app/src/main/java/use_case/trade/TradeInteractor.java
+++ stock-tracker-app/src/main/java/use_case/trade/TradeInteractor.java
@@ -1,6 +1,8 @@
 package use_case.trade;
 
 import entity.TradeTransaction;
+import entity.User;
+import java.time.LocalDateTime;
 
 public class TradeInteractor implements TradeInputBoundary{
     
@@ -12,5 +14,19 @@ public class TradeInteractor implements TradeInputBoundary{
                                                                  tradeInputData.getAmountIn(),
                                                                  tradeInputData.getAmountOut());
         
+    }
+
+    @Override
+    public void execute(TradeInputData tradeInputData) {
+        if (userDataAccessObject.existsByName(tradeInputData.getAmountIn())) {
+            tradePresenter.prepareFailView("PUT SOMETHING HERE");
+        } else {
+            LocalDateTime now =LocalDateTime.now();
+            User user = userFactory.create(tradeInputData.getAmountIn(), tradeInputData.getTradingFee(), now);
+            userDataAccessObject.save(user);
+
+            TradeOutputData tradeOutputData = new TradeOutputData(user.getName(), now.toString(), false);
+            user.Presenter.prepareSuccessView(tradeOutputData);
+        }
     }
 }
