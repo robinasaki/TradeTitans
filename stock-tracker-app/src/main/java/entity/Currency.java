df28e4006679609226b93f184d0335964b0b2eb7
cebwalton <colinwalton2002@gmail.com>
2023-11-12 20:48:34 -0500

    removed some things from Currency that were already covered by Tradeable

--- stock-tracker-app/src/main/java/entity/Currency.java
+++ stock-tracker-app/src/main/java/entity/Currency.java
@@ -4,18 +4,16 @@ import java.util.Date;
 import java.util.HashMap;
 
 public class Currency extends Tradeable {
-    private final String name;
 
-    public Currency(String name, String symbol, HashMap<Date, Double> priceHistory, String currencyName) {
-        super(name, symbol, priceHistory);
-        this.name = currencyName;
+    public Currency(String name, String symbol, HashMap<Date, Double> priceHistory) {
+        super(name, symbol);
+    }
+
+    public Currency(String name) {
+        this(name, null, null);
     }
 
     public void trade() {
         // TODO: Complete trade method.
-    }
-
-    public String getCurrencyName() {
-        return name;
     }
 }
