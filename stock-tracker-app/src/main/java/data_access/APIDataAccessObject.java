4204926e2a4773c44456f5b52cc19caed9acd061
cebwalton <colinwalton2002@gmail.com>
2023-11-12 20:58:38 -0500

    changed api data access object to return historical prices

--- stock-tracker-app/src/main/java/data_access/APIDataAccessObject.java
+++ stock-tracker-app/src/main/java/data_access/APIDataAccessObject.java
@@ -1,13 +1,73 @@
+package data_access;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
+import java.util.HashMap;
+
+//import java.io.InputStreamReader;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
 
 public class APIDataAccessObject {
-        private static final String API_KEY = "../../../../../../key.txt";
+        private static final String API_KEY = "key.txt";
         private static final String BASE_URL = "https://www.alphavantage.co/query";
-        private static final String QUOTE_ENDPOINT = "?function=TIME_SERIES_DAILY&symbol=%s&apikey=%s";
+        private static final String FUNCTION = "TIME_SERIES_DAILY";
+
+        private final HttpClient httpClient;
+        private final ObjectMapper objectMapper;
+
+        public APIDataAccessObject() {
+            this.httpClient = HttpClient.newHttpClient();
+            this.objectMapper = new ObjectMapper();
+        }
+
+        public HashMap<Date, Double> getHistoricalQuotes(String symbol, Date startDate, Date endDate) {
+            HashMap<Date, Double> quotes = new HashMap<>();
+            try {
+                String urlString = buildApiUrl(symbol, startDate, endDate);
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(urlString))
+                        .build();
+
+                HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                JsonNode root = objectMapper.readTree(response.body());
+
+                JsonNode timeSeries = root.get("Time Series (Daily)");
+
+                for (JsonNode node : timeSeries) {
+                    Date date = new SimpleDateFormat("yyyy-MM-dd").parse(node.get("date").asText());
+                    double price = node.get("4. close").asDouble();
+                    quotes.put(date, price);
+                }
+            } catch (IOException | InterruptedException | ParseException e) {
+                e.printStackTrace(); // TODO: handle exception
+            }
+            return quotes;
+        }
+
+        private String buildApiUrl(String symbol, Date startDate, Date endDate) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            String startDateString = dateFormat.format(startDate);
+            String endDateString = dateFormat.format(endDate); 
+            return String.format(
+                    "%s?function=%s&symbol=%s&apikey=%s&outputsize=full&datatype=json&startDate=%s&endDate=%s",
+                    BASE_URL, FUNCTION, symbol, API_KEY, startDateString, endDateString);
+        }
+
+/*
+        }
+
+
 
         public static String getQuote(String symbol) {
             String urlString = BASE_URL + String.format(QUOTE_ENDPOINT, symbol, API_KEY);
@@ -36,4 +96,5 @@ public class APIDataAccessObject {
 
             return null;
     }
+*/
 }
