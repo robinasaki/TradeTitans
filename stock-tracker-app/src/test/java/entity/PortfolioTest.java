f734b5a68d677abc7acdf6fbf07f3c4cde9680f6
cebwalton <colinwalton2002@gmail.com>
2023-11-13 13:07:27 -0500

    finsihed writing test for addTrade()

--- stock-tracker-app/src/test/java/entity/PortfolioTest.java
+++ stock-tracker-app/src/test/java/entity/PortfolioTest.java
@@ -1,8 +1,9 @@
 import org.junit.Assert.*;
-import java.util.Date;
 import entity.Portfolio;
 import entity.Tradeable;
 import org.junit.Test;
+import entity.Transaction;
+import entity.TradeTransaction;
 
 public class PortfolioTest {
     private Portfolio techPortfolio;
@@ -10,6 +11,7 @@ public class PortfolioTest {
     private Tradeable appleStock;
     private Tradeable microsoftStock;
     private Tradeable googleStock;
+    private Tradeable outsidePortfolio; // testing whether we really need BankingTransaction or whether we can use this
 
     public void setup() {
         techPortfolio = new Portfolio("Tech Portfolio");
@@ -17,11 +19,24 @@ public class PortfolioTest {
         appleStock = new Tradeable("Apple Inc.", "AAPL");
         microsoftStock = new Tradeable("Microsoft", "MSFT");
         googleStock = new Tradeable("Alphabet Inc.", "GOOGL");
+        outsidePortfolio = new Tradeable("Outside Portfolio", "");
+        
     }
 
     @Test
-    public void testAddTransaction() {
+    public void testAddTrade() {
         setup();
-        assert(false); // TODO
+
+        // deposit 5000 USD
+        TradeTransaction transaction1 = new TradeTransaction(0,usd, outsidePortfolio, 5000, 0);
+        techPortfolio.addTrade(transaction1);
+
+        // buy 10 shares of Apple for 185 USD each or 1850 USD total
+        TradeTransaction transaction2 = new TradeTransaction(0,appleStock, usd, 10, 1850);
+        techPortfolio.addTrade(transaction2);
+
+        // assert that the portfolio has 10 shares of Apple and 3150 USD
+        assert techPortfolio.getHoldings().get("AAPL") == 10;
+        assert techPortfolio.getHoldings().get("USD") == 3150;
     }
 }
