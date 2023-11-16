5d8214b210eadba74d825cee962b435b13e15798
cebwalton <colinwalton2002@gmail.com>
2023-11-16 18:55:32 -0500

    added line for saving portfolio with FileDataAccessObject

--- stock-tracker-app/src/main/java/use_case/trade/TradeInteractor.java
+++ stock-tracker-app/src/main/java/use_case/trade/TradeInteractor.java
@@ -26,6 +26,7 @@ public class TradeInteractor implements TradeInputBoundary{
                                                       tradeInputData.getTradingFee());
 
         portfolio.addTrade(trade);
+        fileDataAccessObject.savePortfolio(portfolio);
         
     }
 
