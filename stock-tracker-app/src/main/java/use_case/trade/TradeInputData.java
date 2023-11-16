1e1accd8722900e65e799c7d77ff52c4fa96e019
cebwalton <colinwalton2002@gmail.com>
2023-11-16 17:48:20 -0500

    added PortfolioName to TradeInputData

--- stock-tracker-app/src/main/java/use_case/trade/TradeInputData.java
+++ stock-tracker-app/src/main/java/use_case/trade/TradeInputData.java
@@ -3,11 +3,13 @@ package use_case.trade;
 import entity.Tradeable;
 
 public class TradeInputData {
-    final private double tradingFee;
-    final private Tradeable assetIn;
-    final private Tradeable assetOut;
-    final private double amountIn;
-    final private double amountOut;
+    private final String portfolioName;
+    private final double tradingFee;
+    private final Tradeable assetIn;
+    private final Tradeable assetOut;
+    private final double amountIn;
+    private final double amountOut;
+    // TODO: add date
 
     public TradeInputData(double tradingFee, Tradeable assetIn, Tradeable assetOut, double amountIn, double amountOut) {
         this.tradingFee = tradingFee;
@@ -17,6 +19,7 @@ public class TradeInputData {
         this.amountOut = amountOut;
     }
 
+    public String getPortfolioName() { return portfolioName; }
     public double getTradingFee() { return tradingFee; }
     public Tradeable getAssetIn() { return assetIn; }
     public Tradeable getAssetOut() { return assetOut; }
