c71501fc085ba0806642b2e99ab71950d52c41f6
Abdulrahman123109 <free.doom99960@gmail.com>
2023-11-20 14:35:03 -0500

    Adding the Change currency usecase

---
+++ stock-tracker-app/src/main/java/use_case/ChangeDefaultCurrency/ChangeDefaultCurrency.java
@@ -0,0 +1,22 @@
+package use_case.ChangeDefaultCurrency;
+
+import data_access.APIDataAccessObject;
+import data_access.FileDataAccessObject;
+import entity.Currency;
+import entity.Portfolio;
+
+public class ChangeDefaultCurrency {
+
+    private final APIDataAccessObject apiDataAccessObject;
+    private final FileDataAccessObject fileDataAccessObject;
+
+
+    public ChangeDefaultCurrency(APIDataAccessObject apiDataAccessObject, FileDataAccessObject fileDataAccessObject) {
+        this.apiDataAccessObject = apiDataAccessObject;
+        this.fileDataAccessObject = fileDataAccessObject;
+    }
+
+    public void execute (Portfolio portfolio, Currency Newcurrency){
+        portfolio.setCurrency(Newcurrency);
+    }
+}
