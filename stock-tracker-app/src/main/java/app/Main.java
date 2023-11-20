ff0b128854dceffc9ef492a228260cf4d5478932
Chenxu Robin Mao <robin.mao@mail.utoronto.ca>
2023-11-20 14:55:58 -0500

    updates on the DepositView

--- stock-tracker-app/src/main/java/app/Main.java
+++ stock-tracker-app/src/main/java/app/Main.java
@@ -1,7 +1,29 @@
 package app;
 
+import interface_adapter.DepositViewModel;
+import interface_adapter.ViewManagerModel;
+import view.ViewManager;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFrame application = new JFrame("Trade Titans");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        DepositViewModel depositViewModel = new DepositViewModel();
+        // TODO: add more ViewModels here
+
+        // TODO: add more Views, like DepositView, etc.
+        application.pack();
+        application.setVisible(true);
     }
 }
