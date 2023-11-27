a0525ce0b11cc51d92f95498d1d7ef2ba01da3d2
Chenxu Robin Mao <robin.mao@mail.utoronto.ca>
2023-11-27 14:01:21 -0500

    trying to make the program compile, fixes in PortfolioView

---
+++ stock-tracker-app/src/main/java/interface_adapter/portfolio_view/PortfolioViewState.java
@@ -0,0 +1,10 @@
+package interface_adapter.portfolio_view;
+
+import javax.sound.sampled.Port;
+
+public class PortfolioViewState {
+
+    public PortfolioViewState() {
+        // TODO: implement this
+    }
+}
