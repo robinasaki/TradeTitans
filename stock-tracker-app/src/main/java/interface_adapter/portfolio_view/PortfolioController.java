b19fc44917504a17f275d9b53412ce859a6e206b
Abdulrahman123109 <free.doom99960@gmail.com>
2023-11-28 10:25:53 -0500

    implmenting the portfolio view and adding controller and presenter

---
+++ stock-tracker-app/src/main/java/interface_adapter/portfolio_view/PortfolioController.java
@@ -0,0 +1,5 @@
+package interface_adapter.portfolio_view;
+
+public class PortfolioController {
+
+}
--- stock-tracker-app/src/main/java/interface_adapter/portfolio_view/PortfolioViewState.java
+++ stock-tracker-app/src/main/java/interface_adapter/portfolio_view/PortfolioViewState.java
@@ -1,10 +1,27 @@
 package interface_adapter.portfolio_view;
 
+import entity.Portfolio;
+
 import javax.sound.sampled.Port;
+import java.util.List;
 
 public class PortfolioViewState {
+    private List<Portfolio> portfolioList;
 
-    public PortfolioViewState() {
-        // TODO: implement this
+    private Sting FailportfolioView = null;
+
+    public PortfolioViewState(PortfolioViewState copy) {
+            this.portfolioList = copy.portfolioList;
+    }
+
+    public PortfolioViewState(){
+    }
+
+    public Sting getFailportfolioView() {
+        return FailportfolioView;
+    }
+
+    public List<Portfolio> getPortfolioList() {
+        return portfolioList;
     }
 }
