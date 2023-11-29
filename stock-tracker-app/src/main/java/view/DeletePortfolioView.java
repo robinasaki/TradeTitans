afdb67ee7ad84c1bcb9266b2ef80464262c612b4
Abdulrahman123109 <free.doom99960@gmail.com>
2023-11-28 23:12:59 -0500

    minor edits

---
+++ stock-tracker-app/src/main/java/view/DeletePortfolioView.java
@@ -0,0 +1,42 @@
+package view;
+
+import interface_adapter.DeletePortfolio.DeletePortfolioState;
+import interface_adapter.DeletePortfolio.DeletePortfolioViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+
+public class DeletePortfolioView extends JFrame implements ActionListener{
+    private final String ViewName = "Delete Portfolio";
+    private final DeletePortfolioViewModel deletePortfolioViewModel;
+
+    final JButton confirm;
+    final JButton cancel;
+
+    public DeletePortfolioView(DeletePortfolioViewModel deletePortfolioViewModel){
+        this.deletePortfolioViewModel = deletePortfolioViewModel;
+        this.deletePortfolioViewModel.addPropertyChangeListener((PropertyChangeListener) this);
+
+        JLabel title = new JLabel("Delete Portfolio Screen");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JPanel buttons = new JPanel();
+        confirm = new JButton(deletePortfolioViewModel.CONFIRM_BUTTON_LABEL);
+        buttons.add(confirm);
+        cancel = new JButton(deletePortfolioViewModel.CANCEL_BUTTON_LABEL);
+        buttons.add(cancel);
+
+        confirm.addActionListener(this);
+        cancel.addActionListener(this);
+
+        // TODO: not finished yet
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO
+    }
+}
