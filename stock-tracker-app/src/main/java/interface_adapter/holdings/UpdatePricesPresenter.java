06387f4d74e8a4cfc71033a56348185b7b004864
cebwalton <colinwalton2002@gmail.com>
2023-11-29 21:12:49 -0500

    added UpdatePricesPresenter

---
+++ stock-tracker-app/src/main/java/interface_adapter/holdings/UpdatePricesPresenter.java
@@ -0,0 +1,26 @@
+package interface_adapter.holdings;
+
+import use_case.update_prices.UpdatePricesOutputBoundary;
+import use_case.update_prices.UpdatePricesOutputData;
+import interface_adapter.ViewManagerModel;
+
+public class UpdatePricesPresenter implements UpdatePricesOutputBoundary {
+    private final ViewManagerModel viewManagerModel;
+    private final HoldingsViewModel holdingsViewModel;
+
+    public UpdatePricesPresenter(ViewManagerModel viewManagerModel, HoldingsViewModel holdingsViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.holdingsViewModel = holdingsViewModel;
+    }
+
+    public void present(UpdatePricesOutputData outputData) {
+        
+        HoldingsState state = new HoldingsState();
+        state.setSymbols(outputData.getSymbols());
+        state.setPrices(outputData.getPrices());
+        state.setShares(outputData.getShares());
+        state.setValues(outputData.getValues());
+        holdingsViewModel.setState(state);
+        viewManagerModel.setActiveView("holdings");
+    }
+}
