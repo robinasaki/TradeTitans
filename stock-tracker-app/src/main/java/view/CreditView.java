b96e290e55c9cd71bffc9f5397559004f61d9b0c
Chenxu Robin Mao <robin.mao@mail.utoronto.ca>
2023-11-30 18:29:44 -0500

    finished the implementation of the Credit button

--- stock-tracker-app/src/main/java/view/CreditView.java
+++ stock-tracker-app/src/main/java/view/CreditView.java
@@ -5,6 +5,8 @@ import interface_adapter.credit.CreditViewModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CreditView extends JPanel {
     public final String viewName = "credit_view";
@@ -21,10 +23,33 @@ public class CreditView extends JPanel {
         JPanel panel = new JPanel(new GridLayout(0, 1));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        JLabel portfolioSelectionInstruction = new JLabel("Testing");
+        JButton backButton = new JButton("Back");
+        backButton.addActionListener(new backButtonListener());
+        panel.add(backButton);
+
+        // TODO: write more
+        JLabel portfolioSelectionInstruction = new JLabel("<html>" +
+                "Trade Titans is designed for you to explore the stock market with real-life stock data without paying." +
+                "</html>");
         portfolioSelectionInstruction.setFont(new Font("Georgia", Font.PLAIN, 15));
         panel.add(portfolioSelectionInstruction);
 
+        JLabel creditInfo = new JLabel("<html>" +
+                "Made for csc207 @UofT by Chenxu Robin Mao, Jarod Palubiski, Colin Walton and Abdulrahman Mubarak" +
+                "<html/>"
+        );
+        creditInfo.setFont(new Font("Georgia", Font.ITALIC, 10));
+        creditInfo.setForeground(Color.gray);
+        panel.add(creditInfo);
+
         add(panel);
+
+    }
+
+    private class backButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            viewManagerModel.setActiveView("portfolio_selection");
+        }
     }
 }
