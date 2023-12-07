518a67a7eed5e6a3cf8181b94313694e54918d85
Abdulrahman123109 <free.doom99960@gmail.com>
2023-12-06 22:26:42 -0500

    solve a conflict

--- stock-tracker-app/src/main/java/data_access/FileDataAccessObject.java
+++ stock-tracker-app/src/main/java/data_access/FileDataAccessObject.java
@@ -2,6 +2,7 @@ package data_access;
 
 import entity.Portfolio;
 import use_case.delete_portfolio.DeletePortfolioDataAccessInterface;
+import use_case.trade.TradeDataAccessInterface;
 
 import java.io.IOException;
 import java.io.FileNotFoundException;
@@ -12,7 +13,7 @@ import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileDataAccessObject implements DeletePortfolioDataAccessInterface {
+public class FileDataAccessObject implements DeletePortfolioDataAccessInterface, TradeDataAccessInterface {
     private static final String FILE_PATH = "portfolioData.ser";
 
     private void savePortfolios(List<Portfolio> portfolios) {
