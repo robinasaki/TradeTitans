55c5b4ba8a53edd524529be3246db6afbd6bf615
cebwalton <colinwalton2002@gmail.com>
2023-12-06 23:56:22 -0500

    removed duplicated line

--- stock-tracker-app/src/main/java/use_case/trade/TradeDataAccessInterface.java
+++ stock-tracker-app/src/main/java/use_case/trade/TradeDataAccessInterface.java
@@ -2,8 +2,6 @@ package use_case.trade;
 import entity.Portfolio;
 
 public interface TradeDataAccessInterface {
-
-public interface TradeDataAccessInterface {
     public Portfolio getPortfolio(String portfolioName);
     public void removePortfolio(String PortfolioName);
     public void savePortfolio(Portfolio portfolio);
